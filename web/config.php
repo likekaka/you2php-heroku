758aa8e00c00497942722d4710f183d19affa390
demon <331962630@qq.com>
2020-01-28 01:34:14 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -4,6 +4,6 @@ define('APIKEY', 'AIzaSyAFGWfal6797NkpJhvtrHZrrGUP_FT_K_0');
 define('GJ_CODE', 'HK'));
 define('SITE_NAME', 'youtube');
 define('TITLENAME', 'youtube');
-define('EN2DEKEY', 'sxwasdwafik');
+define('EN2DEKEY', 'sxwasdwafi');
 define('EMAIL', 'liuguowai45@gmail.com');
 ?>
