01b9b6559516a1f27acd9b7a2a2b2aa05e376077
demon <331962630@qq.com>
2020-01-28 01:05:20 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY', 'AIzaSyAFGWfal6797NkpJhvtrHZrrGUP_FT_K_0');
 define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
